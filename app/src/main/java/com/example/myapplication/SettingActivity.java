a9fd84ec4d30c75c214dca995d77ca074acb542d
sahilsandhu <sahilsandhu54321@gmail.com>
2020-10-19 15:27:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/SettingActivity.java
@@ -0,0 +1,154 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.transition.CircularPropagation;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.HashMap;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class SettingActivity extends AppCompatActivity {
+
+    Button UpdateAccountSetting;
+    EditText userName, userStatus;
+    CircleImageView userProfileImage;
+
+    private String currentUserID;
+    private FirebaseAuth mAuth;
+
+    private DatabaseReference RootRef;
+
+    private static final int GalleryPick = 1;
+    private StorageReference UserProfileImagesRef;
+    private ProgressDialog loadingBar;
+
+    private Toolbar SettingsToolBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+
+        RootRef = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+
+        InitializeFields();
+
+        userName.setVisibility(View.INVISIBLE);
+     UpdateAccountSetting.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+             UpdateSettings();
+         }
+     });
+
+     RetrieveInformation();
+
+
+    }
+
+    private void RetrieveInformation() {
+        RootRef.child("Users").child(currentUserID)
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot)
+                    {
+                        if ((dataSnapshot.exists()) && (dataSnapshot.hasChild("name") && (dataSnapshot.hasChild("image"))))
+                        {
+                            String retrieveUserName = dataSnapshot.child("name").getValue().toString();
+                            String retrievesStatus = dataSnapshot.child("status").getValue().toString();
+                            String retrieveProfileImage = dataSnapshot.child("image").getValue().toString();
+
+                            userName.setText(retrieveUserName);
+                            userStatus.setText(retrievesStatus);
+                            ///Picasso.get().load(retrieveProfileImage).into(userProfileImage);
+                        }
+                        else if ((dataSnapshot.exists()) && (dataSnapshot.hasChild("name")))
+                        {
+                            String retrieveUserName = dataSnapshot.child("name").getValue().toString();
+                            String retrievesStatus = dataSnapshot.child("status").getValue().toString();
+
+                            userName.setText(retrieveUserName);
+                            userStatus.setText(retrievesStatus);
+                        }
+                        else
+                        {
+                            userName.setVisibility(View.VISIBLE);
+                            Toast.makeText(SettingActivity.this, "Please set & update your profile information...", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+    }
+
+
+    private void InitializeFields() {
+        UpdateAccountSetting = (Button) findViewById(R.id.update_settings_button);
+        userName = (EditText)findViewById(R.id.set_user_name);
+        userStatus = (EditText)findViewById(R.id.set_profile_status);
+        userProfileImage = (CircleImageView) findViewById(R.id.set_profile_image);
+    }
+
+    private void UpdateSettings() {
+        String setUserName = userName.getText().toString();
+        String setStatus = userStatus.getText().toString();
+
+        if (TextUtils.isEmpty(setUserName)) {
+            Toast.makeText(this, "Please write your user name first....", Toast.LENGTH_SHORT).show();
+        }
+        if (TextUtils.isEmpty(setStatus)) {
+            Toast.makeText(this, "Please write your status....", Toast.LENGTH_SHORT).show();
+        } else {
+            HashMap<String, Object> profileMap = new HashMap<>();
+            profileMap.put("uid", currentUserID);
+            profileMap.put("name", setUserName);
+            profileMap.put("status", setStatus);
+            RootRef.child("Users").child(currentUserID).updateChildren(profileMap)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                SendUserToMainActivity();
+                                Toast.makeText(SettingActivity.this, "Profile Updated Successfully...", Toast.LENGTH_SHORT).show();
+                            } else {
+                                String message = task.getException().getMessage();
+                                Toast.makeText(SettingActivity.this, "Error: " + message, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+    }
+        private void SendUserToMainActivity() {
+            Intent mainIntent = new Intent(SettingActivity.this, MainActivity.class);
+            mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(mainIntent);
+        }
+
+
+}
